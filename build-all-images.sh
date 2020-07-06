c70c54f69a8a1b5e803935053627e0ba645a5e19
Florian Piesche <florian@yellowkeycard.net>
2020-07-06 22:56:06 +0100

    Make Dockerfile use appropriate architecture for cross-platform builds.

--- build-all-images.sh
+++ build-all-images.sh
@@ -21,8 +21,7 @@ for mode in "${servermodes[@]}"; do
             if [[ $? -eq 0 ]]; then
                 echo "Building Docker image for hercules_${mode}_packetver-${packetver:-default}_${arch}..."
                 cd hercules_${mode}_packetver-${packetver:-${PACKETVER_FROM_SOURCE}}_${arch}
-                echo "Now in "`pwd`
-                docker build . --tag=florianpiesche/${DOCKER_REPO}:${arch}
+                docker build . --tag=florianpiesche/${DOCKER_REPO}:${arch} --build-arg ARCH=${arch}
                 echo "Pushing image to Docker Hub as florianpiesche/${DOCKER_REPO}:${arch}"
                 docker push florianpiesche/${DOCKER_REPO}:${arch}
                 docker manifest create --amend florianpiesche/${DOCKER_REPO}:latest florianpiesche/${DOCKER_REPO}:${arch}
