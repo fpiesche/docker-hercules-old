d463a737c876faf43a54d430c3050f51afc40ddc
Florian Piesche <florian@yellowkeycard.net>
2020-07-14 00:11:21 +0100

    Remove now-unnecessary shell script to create inter-server user.

--- distrib-tmpl/hercules/sql-files/9-interserver-user.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "USE ragnarok; UPDATE login set userid = '${INTERSERVER_USER}', user_pass = '${INTERSERVER_PASSWORD}' where account_id = 1;"
